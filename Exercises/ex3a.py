f9427781b74f045dd532865b82a6c3c09cacc596
Jose_ <joseamsilva100@gmail.com>
2022-07-02 18:57:47 +0100

    Exercise 3 a) Attempts.

--- Exercises/ex3a.py
+++ Exercises/ex3a.py
@@ -9,25 +9,30 @@ def insert_bits_into(bits, container, max, min):
         bits.append(0)
 
 
-def NRZU_Coder(binary_sequence, num_of_images_per_bits, max, min):
+def NRZU_Coder(binary_sequence, t, max, min):
     length = len(binary_sequence)
-    n = num_of_images_per_bits
-    emitter = [] * (length * n)
+
+    emitter = [] * (length * t)
     for bit in binary_sequence:
         if bit == 1:
-            n = num_of_images_per_bits
+            n = t
             while n > 0:
                 emitter.append(max)
                 n -= 1
         else:
-            n = num_of_images_per_bits
+            n = t
             while n > 0:
                 emitter.append(min)
                 n -= 1
+
+    plt.title("Unipolar NRZ")
+    plt.plot(emitter)
+    plt.show()
+
     return emitter
 
 
-def NRZU_Decoder(binary_sequence, num_of_images_per_bits, max, min):
+def NRZU_Decoder(binary_sequence, t, max, min):
     length = len(binary_sequence)
     code = []
     container = []
@@ -39,7 +44,7 @@ def NRZU_Decoder(binary_sequence, num_of_images_per_bits, max, min):
             break
 
         bit = binary_sequence[idx]
-        if len(container) < num_of_images_per_bits:
+        if len(container) < t:
             container.append(bit)
         else:
             insert_bits_into(code, container, max, min)
@@ -47,17 +52,32 @@ def NRZU_Decoder(binary_sequence, num_of_images_per_bits, max, min):
 
         idx += 1
 
-    if len(code) == int(length / num_of_images_per_bits):
+    if len(code) == int(length / t):
         return code
     else:
-        return "An error has occurred on decode."
+        return "An error has occurred on NRZU decode."
 
 
 def main():
-    coded = NRZU_Coder([1, 0, 0, 1, 0, 1, 1], 10, 5, 0)
+    coded = NRZU_Coder([1, 0, 0, 1, 1, 0, 1], 10, 5, 0)
     print(coded)
-    decoded = NRZU_Decoder(coded, 10, 5, 0)
-    print(decoded)
+    # unipolarNRZ = []
+    # for x in coded:
+    #     for i in range(100):
+    #         unipolarNRZ.append(x)
+    # decoded = NRZU_Decoder(coded, 10, 5, 0)
+    # print(decoded)
+    rng = np.arange(50)
+    rnd = np.random.randint(0, 10, size=(3, rng.size))
+    yrs = 1950 + rng
+
+    fig, ax = plt.subplots(figsize=(5, 3))
+    ax.stackplot(yrs, rng + rnd, labels=['Eastasia', 'Eurasia', 'Oceania'])
+    ax.set_title('Combined debt growth over time')
+    ax.legend(loc='upper left')
+    ax.set_ylabel('Total debt')
+    ax.set_xlim(xmin=yrs[0], xmax=yrs[-1])
+    fig.tight_layout()
 
 
 if __name__ == '__main__':
